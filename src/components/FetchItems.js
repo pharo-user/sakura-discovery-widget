f7f299438c8767ef60e58e9c3a7927fa1478572c
Muazzam Kazmi <muazzam_ali@live.com>
2022-04-08 23:02:33 +0500

    Shuffle products randomly

--- src/components/FetchItems.js
+++ src/components/FetchItems.js
@@ -19,6 +19,18 @@
     return "";
   }
 
+  function shuffle(items) {
+    let i = items.length;
+    let rnd;
+  
+    while (i) {
+      rnd = Math.floor(Math.random() * i);
+      i--;
+      [items[i], items[rnd]] = [items[rnd], items[i]];
+    }
+  
+    return items;
+  }  
 
   export async function fetchById(base_url, mode, id) {
     //return [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], ""];
@@ -37,6 +49,7 @@
             art
           )
         };
+        items = shuffle(items);
         console.log(items);
         return [items,decode_i18n(d.title)];
       })
